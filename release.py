7e119494ca6501a9cda92b7c4eda304feeafbd6e
Zamiell <5511220+Zamiell@users.noreply.github.com>
2020-10-21 08:54:31 -0400

    v1.0.7

--- release.py
+++ release.py
@@ -1,6 +1,6 @@
 #!/c/Python38/python.exe
 
-""" This script handles some miscellaneous tasks when releasing a new version of The Babies Mod. """
+""" This script handles some miscellaneous tasks when releasing a new version of the mod. """
 
 # Standard imports
 import sys
@@ -13,6 +13,14 @@ if (sys.version_info < (3, 0)):
      print('This script requires Python 3.')
      sys.exit(1)
 
+# Constants
+DIR = os.path.dirname(os.path.realpath(__file__))
+MOD_SOURCE_DIR = os.path.join(DIR, 'mod')
+MAIN_LUA = 'main.lua'
+MODS_DIR = 'C:\\Users\\james\\Documents\\My Games\\Binding of Isaac Afterbirth+ Mods'
+PROJECT_NAME = os.path.basename(DIR)
+MOD_TARGET_DIR = os.path.join(MODS_DIR, PROJECT_NAME)
+
 # Subroutines
 def error(message, exception=None):
     if exception is None:
@@ -23,12 +31,9 @@ def error(message, exception=None):
 
 # Increment the version in the "constants.ts" file
 # http://stackoverflow.com/questions/17140886/how-to-search-and-replace-text-in-a-file-using-python
-DIR = os.path.dirname(os.path.realpath(__file__))
 CONSTANTS_TS_FILE = os.path.join(DIR, 'src', 'constants.ts')
 with open(CONSTANTS_TS_FILE, 'r') as file_handle:
     FILE_DATA = file_handle.read()
-
-# Replace the target string
 NEW_FILE = ''
 VERSION_PREFIX = 'v1.0.'
 VERSION = ''
@@ -40,8 +45,6 @@ for line in iter(FILE_DATA.splitlines()):
         NEW_FILE += 'export const VERSION = "' + VERSION + '";\n'
     else:
         NEW_FILE += line + '\n'
-
-# Write the file out again
 if VERSION == '':
     print("Failed to parse the version.")
     sys.exit(1)
@@ -56,21 +59,62 @@ if RETURN_CODE != 0:
     error('Failed to compile.')
 
 # Copy it
-MOD_SOURCE_DIR = os.path.join(DIR, 'mod')
-SOURCE_MAIN_LUA = os.path.join(MOD_SOURCE_DIR, 'main.lua')
-MODS_DIR = 'C:\\Users\\james\\Documents\\My Games\\Binding of Isaac Afterbirth+ Mods'
-PROJECT_NAME = os.path.basename(DIR)
-MOD_TARGET_DIR = os.path.join(MODS_DIR, PROJECT_NAME)
-TARGET_MAIN_LUA = os.path.join(MOD_TARGET_DIR, 'main.lua')
+SOURCE_MAIN_LUA = os.path.join(MOD_SOURCE_DIR, MAIN_LUA)
+TARGET_MAIN_LUA = os.path.join(MOD_TARGET_DIR, MAIN_LUA)
 try:
     shutil.copy(SOURCE_MAIN_LUA, TARGET_MAIN_LUA)
 except Exception as err:
     error('Failed to copy the "' + SOURCE_MAIN_LUA + '" to "' + TARGET_MAIN_LUA + '":', err)
 
+# Also write out the version to the "metadata.xml" file
+METADATA_XML = 'metadata.xml'
+METADATA_XML_PATH = os.path.join(MOD_SOURCE_DIR, METADATA_XML)
+with open(METADATA_XML_PATH, 'r') as file_handle:
+    FILE_DATA = file_handle.read()
+NEW_FILE = ''
+for line in iter(FILE_DATA.splitlines()):
+    match = re.search(r'<version>', line)
+    if match:
+        NEW_FILE += '    <version>' + VERSION + '</version>\n'
+    else:
+        NEW_FILE += line + '\n'
+with open(METADATA_XML_PATH, 'w', newline='\n') as file:
+    file.write(NEW_FILE)
+
+# Copy it
+TARGET_METADATA_XML_PATH = os.path.join(MOD_TARGET_DIR, METADATA_XML)
+try:
+    shutil.copy(METADATA_XML_PATH, TARGET_METADATA_XML_PATH)
+except Exception as err:
+    error('Failed to copy the "' + METADATA_XML_PATH + '" to "' + TARGET_METADATA_XML_PATH + '":', err)
+
 # Also write out the version to the "version.txt" file
-VERSION_FILE = os.path.join(MOD_SOURCE_DIR, 'version.txt')
-with open(VERSION_FILE, 'w', newline='\n') as file:
+VERSION_TXT = 'version.txt'
+VERSION_TXT_PATH = os.path.join(MOD_SOURCE_DIR, VERSION_TXT)
+with open(VERSION_TXT_PATH, 'w', newline='\n') as file:
     file.write(VERSION)
+
+# Copy it
+TARGET_VERSION_TXT_PATH = os.path.join(MOD_TARGET_DIR, VERSION_TXT)
+try:
+    shutil.copy(VERSION_TXT_PATH, TARGET_VERSION_TXT_PATH)
+except Exception as err:
+    error('Failed to copy the "' + VERSION_TXT_PATH + '" to "' + TARGET_VERSION_TXT_PATH + '":', err)
+
+# Also write out the version to the "package.json" file
+PACKAGE_JSON_PATH = os.path.join(DIR, 'package.json')
+with open(PACKAGE_JSON_PATH, 'r') as file_handle:
+    FILE_DATA = file_handle.read()
+NEW_FILE = ''
+for line in iter(FILE_DATA.splitlines()):
+    match = re.search(r'"version": "', line)
+    if match:
+        VERSION_JUST_NUMBERS = VERSION[1:]
+        NEW_FILE += '  "version": "' + VERSION_JUST_NUMBERS + '",\n'
+    else:
+        NEW_FILE += line + '\n'
+with open(METADATA_XML_PATH, 'w', newline='\n') as file:
+    file.write(NEW_FILE)
 
 # Remove the "disable.it" file, if present
 DISABLE_IT_PATH = os.path.join(MOD_TARGET_DIR, 'disable.it')
